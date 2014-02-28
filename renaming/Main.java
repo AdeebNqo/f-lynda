612d0e03515857e519761e390dd31836a487867b
AdeebNqo <adeebnqo@gmail.com>
2014-02-28 17:46:36 +0200

    can list all video files

--- renaming/Main.java
+++ renaming/Main.java
@@ -3,8 +3,17 @@
 Class for renaming the actual files
 
 */
+import java.io.File;
 public class Main{
 	public static void main(String[] args){
-			
+		try{
+			String path = args[0];
+			File vidDirectory = new File(path);
+			for (File video : vidDirectory.listFiles()){
+				System.out.println(video);
+			}	
+		}catch(Exception e){
+
+		}		
 	}
 }
